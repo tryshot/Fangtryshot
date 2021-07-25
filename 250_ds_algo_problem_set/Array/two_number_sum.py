93f086c236a64ef0b8bf60e096d6ef27f2c2c3cc
tryshot <aruna.menda250@gmail.com>
2021-07-25 21:45:07 +0530

    first commit
    Awesome

--- 250_ds_algo_problem_set/Array/two_number_sum.py
+++ 250_ds_algo_problem_set/Array/two_number_sum.py
@@ -1,1 +1,9 @@
-print ('first')
+# function that takes in a non-empty array of distinct integers and an
+# integer representing a target sum. If any two numbers in the input array sum
+# up to the target sum, the function should return them in an array, or else return
+# an empty array.
+# Note : target sum with 2 distinct Integers only
+
+  
+# [-1, 2, 3, 5, 7, 8, 11]  = 14
+#                    
